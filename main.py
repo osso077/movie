e2a10efd67af2694a3a5c4f190de0b602ede8b27
osso077 <dunguma.3322@gmail.com>
2026-09-15 14:43:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 import streamlit as st
-
-st.title("첫 배포 확인 👋")
-st.write("여기까지 보이면 배포 성공입니다.")
-
-import streamlit as st
 import pandas as pd
 import requests
 from datetime import datetime, timedelta
